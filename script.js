da0d4b16a563874d02ac3c06ce969415be71f02c
Christian Almli <chrisalm97@gmail.com>
2020-03-15 23:45:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// question 1
+// my answer for question 1
+var petArray = ["Cat", "Cow", "Dog"];
+
+// question 2
+// my answer for question 2
+console.log(petArray[1]);
+
+// question 3
+// my answer for question 3
+petArray.push("sheep");
+
+// question 4
+// my answer for question 4
+console.log(petArray.length);
+
+// question 5
+// my answer for question 5
+var catObject = {
+    name: "jack",
+     colour: "black",
+      age: "6"
+    };
+
+// question 6
+// my answer for question 6
+console.log(catObject[2]);
+
+// question 7
+// my answer for question 7
+var catArray = [
+    {
+        name: "jack",
+        colour: "black",
+        age: 6
+    }
+];
+
+// question 8
+// my answer for question 8
+for(i = 0; i < catArray.length; i++);
+
+console.log(catArray[i].name);
+
+console.log(catArray[i].colour);
+
+console.log(catArray[i].age);
+
+// question 9
+// my answer for question 9
+function logToConsole(theAnimal) {
+    console.log("donkey");
+}
+
+logToConsole();
+
+// question 10
+// my answer for question 10
+function logToConsole(theAnimal) {
+    console.log(theAnimal);
+}
+
+logToConsole("donkey");
