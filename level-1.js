6235d62b69b2fe4fb3e55cd879525aa54db56203
Christian Almli <chrisalm97@gmail.com>
2020-03-22 21:44:45 +0100

    some sort of commit message

---
+++ level-1.js
@@ -0,0 +1,101 @@
+function main() {
+    return 'Hello, World!';
+  }
+  
+  main();
+  
+  // question 1
+  
+  // my answer for question 1
+  var myName = "Christian Almli";
+  
+  // question 2
+  
+  // my answer for question 2
+  var person = {firstName:"Patrick", age:27};
+  
+  // question 3
+  
+  // my answer for question 3
+  var outOfStock = "true";
+  
+  if (outOfStock == true) {
+      console.log("Out of stock");
+  }
+  else {
+      console.log("In stock");
+  }
+  
+  // question 4
+  
+  // my answer for question 4
+  var numbers = ["4", "12", "19", "29"];
+  
+  for (var i=0; i<numbers.length; i++) {
+      console.log(numbers [i]);
+  }
+  
+  // question 5
+  
+  // my answer for question 5
+  for (var i = 15; i < 25; i++) {
+      console.log(i);
+  }
+  // question 6
+  
+  // my answer for question 6
+  for (i = 15; i < 25; i++) {
+      if (i === 20) {
+          console.log(i);
+      }
+  }
+      
+  // question 7
+  
+  // my answer for question 7
+  var tickets = [
+      {
+          bandName: "TOOL",
+          availableSeats: 145,
+          boughtByUser: false
+      },
+      {
+          bandName: "Rival Sons",
+          availableSeats: 208,
+          boughtByUser: true
+      }
+  ];
+  
+  for (var i = 0; i < tickets.length; i++) {
+  
+      console.log(tickets[i].availableSeats);
+      console.log(tickets[i].boughtByUser);
+  }
+  
+  // question 8
+  
+  // my answer for question 8
+  function whatIDontLike(insect) {
+      console.log("I don't like " + insect);
+  }
+  
+  whatIDontLike("spiders");
+  
+  // question 9
+  
+  // my answer for question 9
+  function sub(first, second) {
+      var away = first + second;
+      console.log();
+  }
+  // question 10
+  
+  // my answer for question 10
+  var emptyArr = [
+  
+  ];
+  
+  function myFunc() {
+  
+  
+  }
